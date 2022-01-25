7dd89e39f3b0f17add7f491e5221eb422260fe61
Whitedragon1987 <vanoostenjohan@hotmail.com>
2022-01-25 13:39:11 +0100

    first commit

---
+++ src/main/java/nl/novi/techiteasy1121/TechItEasy1121Application.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasy1121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasy1121Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechItEasy1121Application.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/techiteasy1121/TechItEasy1121ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasy1121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasy1121ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
