5763eb680225c9a19ac66a5b12c75e9e928f3d14
Johan van Oosten <80273090+Whitedragon1987@users.noreply.github.com>
2022-05-30 13:03:50 +0200

    Update ExceptionController.java

--- src/main/java/nl/novi/techiteasy1121/controllers/ExceptionController.java
+++ src/main/java/nl/novi/techiteasy1121/controllers/ExceptionController.java
@@ -1,8 +1,6 @@
 package nl.novi.techiteasy1121.controllers;
 
-import nl.novi.techiteasy1121.exceptions.BadRequestException;
 import nl.novi.techiteasy1121.exceptions.RecordNotFoundException;
-import nl.novi.techiteasy1121.exceptions.UsernameNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,18 +16,5 @@ public class ExceptionController {
 
     }
 
-    @ExceptionHandler(value = UsernameNotFoundException.class)
-    public ResponseEntity<Object> exception(UsernameNotFoundException exception) {
-
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-
-    }
-
-    @ExceptionHandler(value = BadRequestException.class)
-    public ResponseEntity<Object> exception(BadRequestException exception) {
-
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-
-    }
 
 }
