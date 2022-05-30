1fab5931be10bb1d275d2a8c9cd8e4e423d6c604
Johan van Oosten <80273090+Whitedragon1987@users.noreply.github.com>
2022-05-30 11:42:36 +0200

    Delete AuthorityKey.java

--- src/main/java/nl/novi/techiteasy1121/models/AuthorityKey.java
+++
@@ -1,8 +0,0 @@
-package nl.novi.techiteasy1121.models;
-
-import java.io.Serializable;
-
-public class AuthorityKey implements Serializable {
-    private String username;
-    private String authority;
-}
