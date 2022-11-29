12571fecb11b1ee4472c30f43889b3f03d96c7f5
MRensen <markrensen@hotmail.com>
2022-11-29 13:41:27 +0100

    added validation

--- src/main/java/nl/novi/techiteasy1121/dtos/TelevisionInputDto.java
+++ src/main/java/nl/novi/techiteasy1121/dtos/TelevisionInputDto.java
@@ -1,169 +1,177 @@
 package nl.novi.techiteasy1121.dtos;
 
+import javax.validation.constraints.*;
+
 public class TelevisionInputDto {
 
-        private String type;
-        private String brand;
-        private String name;
-        private Double price;
-        private Double availableSize;
-        private Double refreshRate;
-        private String screenType;
-        private String screenQuality;
-        private Boolean smartTv;
-        private Boolean wifi;
-        private Boolean voiceControl;
-        private Boolean hdr;
-        private Boolean bluetooth;
-        private Boolean ambiLight;
-        private Integer originalStock;
-        private Integer sold;
+    @NotNull(message = "Type is required") // Type moet ingevuld verplicht worden in je JSON, je krijgt een message als je dit niet doet.
+    private String type;
+    @NotNull(message = "Brand is required")
+    private String brand;
+    @Size(max = 20, message = "Name must be between 0-20 characters") // maximale lengte van de string, min is automatisch 0.
+    private String name;
+    @Positive(message = "Price must be higher than zero")
+    private Double price;
+    private Double availableSize;
+    private Double refreshRate;
+    private String screenType;
+    private String screenQuality;
+    private Boolean smartTv;
+    private Boolean wifi;
+    private Boolean voiceControl;
+    @AssertTrue(message = "All television must be hdr minimum")
+    private Boolean hdr;
+    private Boolean bluetooth;
+    private Boolean ambiLight;
+    @PositiveOrZero(message = "Television cannot have negative stock")
+    private Integer originalStock;
+    private Integer sold;
 
-        public TelevisionInputDto(String type, String brand, String name, Double price, Double availableSize, Double refreshRate, String screenType, String screenQuality, Boolean smartTv, Boolean wifi, Boolean voiceControl, Boolean hdr, Boolean bluetooth, Boolean ambiLight, Integer originalStock, Integer sold) {
-            this.type = type;
-            this.brand = brand;
-            this.name = name;
-            this.price = price;
-            this.availableSize = availableSize;
-            this.refreshRate = refreshRate;
-            this.screenType = screenType;
-            this.screenQuality = screenQuality;
-            this.smartTv = smartTv;
-            this.wifi = wifi;
-            this.voiceControl = voiceControl;
-            this.hdr = hdr;
-            this.bluetooth = bluetooth;
-            this.ambiLight = ambiLight;
-            this.originalStock = originalStock;
-            this.sold = sold;
-        }
+    public TelevisionInputDto(String type, String brand, String name, Double price, Double availableSize, Double refreshRate, String screenType, String screenQuality, Boolean smartTv, Boolean wifi, Boolean voiceControl, Boolean hdr, Boolean bluetooth, Boolean ambiLight, Integer originalStock, Integer sold) {
+        this.type = type;
+        this.brand = brand;
+        this.name = name;
+        this.price = price;
+        this.availableSize = availableSize;
+        this.refreshRate = refreshRate;
+        this.screenType = screenType;
+        this.screenQuality = screenQuality;
+        this.smartTv = smartTv;
+        this.wifi = wifi;
+        this.voiceControl = voiceControl;
+        this.hdr = hdr;
+        this.bluetooth = bluetooth;
+        this.ambiLight = ambiLight;
+        this.originalStock = originalStock;
+        this.sold = sold;
+    }
 
-        public String getType() {
-            return type;
-        }
+    public String getType() {
+        return type;
+    }
 
-        public String getBrand() {
-            return brand;
-        }
+    public String getBrand() {
+        return brand;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public Double getPrice() {
-            return price;
-        }
+    public Double getPrice() {
+        return price;
+    }
 
-        public Double getAvailableSize() {
-            return availableSize;
-        }
+    public Double getAvailableSize() {
+        return availableSize;
+    }
 
-        public Double getRefreshRate() {
-            return refreshRate;
-        }
+    public Double getRefreshRate() {
+        return refreshRate;
+    }
 
-        public String getScreenType() {
-            return screenType;
-        }
+    public String getScreenType() {
+        return screenType;
+    }
 
-        public String getScreenQuality() {
-            return screenQuality;
-        }
+    public String getScreenQuality() {
+        return screenQuality;
+    }
 
-        public Boolean getSmartTv() {
-            return smartTv;
-        }
+    public Boolean getSmartTv() {
+        return smartTv;
+    }
 
-        public Boolean getWifi() {
-            return wifi;
-        }
+    public Boolean getWifi() {
+        return wifi;
+    }
 
-        public Boolean getVoiceControl() {
-            return voiceControl;
-        }
+    public Boolean getVoiceControl() {
+        return voiceControl;
+    }
 
-        public Boolean getHdr() {
-            return hdr;
-        }
+    public Boolean getHdr() {
+        return hdr;
+    }
 
-        public Boolean getBluetooth() {
-            return bluetooth;
-        }
+    public Boolean getBluetooth() {
+        return bluetooth;
+    }
 
-        public Boolean getAmbiLight() {
-            return ambiLight;
-        }
+    public Boolean getAmbiLight() {
+        return ambiLight;
+    }
 
-        public Integer getOriginalStock() {
-            return originalStock;
-        }
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
 
-        public Integer getSold() {
-            return sold;
-        }
+    public Integer getSold() {
+        return sold;
+    }
 
-        public void setType(String type) {
-            this.type = type;
-        }
+    public void setType(String type) {
+        this.type = type;
+    }
 
-        public void setBrand(String brand) {
-            this.brand = brand;
-        }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setPrice(Double price) {
-            this.price = price;
-        }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
-        public void setAvailableSize(Double availableSize) {
-            this.availableSize = availableSize;
-        }
+    public void setAvailableSize(Double availableSize) {
+        this.availableSize = availableSize;
+    }
 
-        public void setRefreshRate(Double refreshRate) {
-            this.refreshRate = refreshRate;
-        }
+    public void setRefreshRate(Double refreshRate) {
+        this.refreshRate = refreshRate;
+    }
 
-        public void setScreenType(String screenType) {
-            this.screenType = screenType;
-        }
+    public void setScreenType(String screenType) {
+        this.screenType = screenType;
+    }
 
-        public void setScreenQuality(String screenQuality) {
-            this.screenQuality = screenQuality;
-        }
+    public void setScreenQuality(String screenQuality) {
+        this.screenQuality = screenQuality;
+    }
 
-        public void setSmartTv(Boolean smartTv) {
-            this.smartTv = smartTv;
-        }
+    public void setSmartTv(Boolean smartTv) {
+        this.smartTv = smartTv;
+    }
 
-        public void setWifi(Boolean wifi) {
-            this.wifi = wifi;
-        }
+    public void setWifi(Boolean wifi) {
+        this.wifi = wifi;
+    }
 
-        public void setVoiceControl(Boolean voiceControl) {
-            this.voiceControl = voiceControl;
-        }
+    public void setVoiceControl(Boolean voiceControl) {
+        this.voiceControl = voiceControl;
+    }
 
-        public void setHdr(Boolean hdr) {
-            this.hdr = hdr;
-        }
+    public void setHdr(Boolean hdr) {
+        this.hdr = hdr;
+    }
 
-        public void setBluetooth(Boolean bluetooth) {
-            this.bluetooth = bluetooth;
-        }
+    public void setBluetooth(Boolean bluetooth) {
+        this.bluetooth = bluetooth;
+    }
 
-        public void setAmbiLight(Boolean ambiLight) {
-            this.ambiLight = ambiLight;
-        }
+    public void setAmbiLight(Boolean ambiLight) {
+        this.ambiLight = ambiLight;
+    }
 
-        public void setOriginalStock(Integer originalStock) {
-            this.originalStock = originalStock;
-        }
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
 
-        public void setSold(Integer sold) {
-            this.sold = sold;
-        }
+    public void setSold(Integer sold) {
+        this.sold = sold;
+    }
 }
 
